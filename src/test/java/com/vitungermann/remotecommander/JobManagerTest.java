f929b3aba130f5c9447f3622ea479f140c72b1d4
VitekU <ungermann.vit@gmail.com>
2026-03-15 23:54:32 +0100

    (fix): remove unused statement

--- src/test/java/com/vitungermann/remotecommander/JobManagerTest.java
+++ src/test/java/com/vitungermann/remotecommander/JobManagerTest.java
@@ -1,6 +1,5 @@
 package com.vitungermann.remotecommander;
 
-import com.vitungermann.remotecommander.helperstructs.JobStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
